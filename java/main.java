a19a60f4529c36b8d722c1a3d0593a5ed16b0dd4
Joan Bensulan <143674667+JoanGwapa143@users.noreply.github.com>
2023-09-29 15:23:02 +0800

    Add files via upload
    mc1

---
+++ java/main.java
@@ -0,0 +1,21 @@
+
+public class info {
+
+  public static void main(String[] args) {
+   
+   
+ 
+       
+  
+        student student1 = new student("joan benulan", "IT24C", "help remaind you what the original purpose of your life is");
+        
+        student1.displayDetails();
+       
+       
+       
+       System.out.println("my info ");
+       student1.displayDetails();
+       
+    }
+}
+
